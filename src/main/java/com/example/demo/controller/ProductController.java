6a5228c1af86030a40f3c63fd389158320560f97
MorozovME25 <127974204+MorozovME25@users.noreply.github.com>
2025-05-24 21:32:37 +0300

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return List.of(new Product(1L, "Test Product"));
+    }
+}
